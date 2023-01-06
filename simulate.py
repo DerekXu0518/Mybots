416e8fae8980bcb42dce9069a2c0e7f5da7d423a
Sunyang Xu <derekxu0518@gmail.com>
2023-01-06 14:34:55 -0600

    finish onelink

--- simulate.py
+++ simulate.py
@@ -2,9 +2,9 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
-for t in range(1,1000):
+p.loadSDF("box.sdf")
+for t in range(1, 1000):
     p.stepSimulation()
     time.sleep(1/60)
     print(t)
 p.disconnect()
-
