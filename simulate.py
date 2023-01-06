f83743890253e65f6d5e57075bd2c87ba7e3856c
Sunyang Xu <derekxu0518@gmail.com>
2023-01-06 15:29:34 -0600

    create many links

--- simulate.py
+++ simulate.py
@@ -1,10 +1,16 @@
+# import necessary conditions
 import pybullet as p
 import time
+import pybullet_data
 
-physicsClient = p.connect(p.GUI)
+physicsClient= p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0, 0, -9.8)
+planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 for t in range(1, 1000):
     p.stepSimulation()
-    time.sleep(1/60)
+    time.sleep(1 / 60)
     print(t)
 p.disconnect()
