21fd318e8810329a4f4f1ccf18e91d4fc95059b5
Sunyang Xu <derekxu0518@gmail.com>
2023-01-06 12:03:51 -0600

    add time to sleep

--- simulate.py
+++ simulate.py
@@ -1,5 +1,10 @@
 import pybullet as p
-physicsClient = p.connect(p.GUI)
+import time
 
+physicsClient = p.connect(p.GUI)
+for t in range(1,1000):
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(t)
 p.disconnect()
 
