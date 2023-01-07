8e0885b1c5334e810feca48d312400c950435722
Sunyang Xu <derekxu0518@gmail.com>
2023-01-06 19:58:06 -0600

    make joints

--- generate.py
+++ generate.py
@@ -1,10 +1,20 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
-for x in range(0, 10):
-    for y in range (0, 3):
-        for z in range (0, 3):
-            pyrosim.Send_Cube(name="Box", pos=[y, z, 0.5+x], size=[1*0.9**x, 1*0.9**x, 1*0.9**x])
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[3, 3, 0.5], size=[1, 1, 1])
+    pyrosim.End()
 
-pyrosim.End()
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="torso", pos=[1.5, 0, 1.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="torso_backleg", parent="torso", child="backleg", type="revolute", position=[1, 0, 1])
+    pyrosim.Send_Cube(name="backleg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="torso_frontleg", parent="torso", child="frontleg", type="revolute", position=[2, 0, 1])
+    pyrosim.Send_Cube(name="frontleg", pos=[0.5, 0, -0.5], size=[1, 1, 1])
+
+    pyrosim.End()
+
+#Call both functions
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -7,8 +7,11 @@ physicsClient= p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0, 0, -9.8)
+
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
+robotId = p.loadURDF("body.urdf")
+
+p.loadSDF("world.sdf")
 for t in range(1, 1000):
     p.stepSimulation()
     time.sleep(1 / 60)
