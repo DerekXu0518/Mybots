e37a507972645ece67542935278b78d26fb98b8f
Sunyang Xu <derekxu0518@gmail.com>
2023-01-11 15:26:41 -0600

    create tiptoe static motor

--- generate.py
+++ generate.py
@@ -1,9 +1,11 @@
 import pyrosim.pyrosim as pyrosim
 
+
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[3, 3, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Box", pos=[-2, 2, 0.5], size=[1, 1, 1])
     pyrosim.End()
+
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
@@ -15,6 +17,7 @@ def Create_Robot():
 
     pyrosim.End()
 
-#Call both functions
+
+# Call both functions
 Create_World()
 Create_Robot()
--- simulate.py
+++ simulate.py
@@ -6,7 +6,7 @@ import pyrosim.pyrosim as pyrosim
 import numpy
 
 # see computer conditions
-physicsClient= p.connect(p.GUI)
+physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0, 0, -9.8)
@@ -17,14 +17,28 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = numpy.zeros(100)
-frontLegSensorValues = numpy.zeros(100)
+backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
 
-for i in range(0, 100):
+for i in range(0, 1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    time.sleep(1 / 60)
+    # backleg joint motor
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robotId,
+        jointName=b"Torso_BackLeg",
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=-numpy.pi/8.0,
+        maxForce=500)
+    # frontleg joint motor
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robotId,
+        jointName=b"Torso_FrontLeg",
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=numpy.pi/8.0,
+        maxForce=500)
+    time.sleep(1 / 120)
 
 p.disconnect()
 
