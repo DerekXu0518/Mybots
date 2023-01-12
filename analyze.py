0970143eaa273fa4076c09389a5026892cbad8ac
Sunyang Xu <derekxu0518@gmail.com>
2023-01-11 20:08:46 -0600

    make the robot move one direction

--- analyze.py
+++ analyze.py
@@ -1,13 +1,17 @@
 import numpy
 import matplotlib.pyplot
 
-# retrieve sensor value
+# retrieve .npy files
 backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
 frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
+backLegTargetAngles = numpy.load("data/backLegTargetAngles.npy")
+frontLegTargetAngles = numpy.load("data/frontLegTargetAngles.npy")
 
 # plot data
-line1 = matplotlib.pyplot.plot(backLegSensorValues, linewidth=4, label="backLegSensorValues")
-line2 = matplotlib.pyplot.plot(frontLegSensorValues, label="frontLegSensorValues")
+matplotlib.pyplot.plot(backLegTargetAngles, label="backLeg")
+matplotlib.pyplot.plot(frontLegTargetAngles, label="frontLeg")
+#line1 = matplotlib.pyplot.plot(backLegSensorValues, linewidth=4, label="backLegSensorValues")
+#line2 = matplotlib.pyplot.plot(frontLegSensorValues, label="frontLegSensorValues")
 
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
