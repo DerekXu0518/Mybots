301e835c020155a3d3ef1f08738c0eb7103685d1
Sunyang Xu <derekxu0518@gmail.com>
2023-01-18 17:09:27 -0600

    Solve step 76 issue and finish part I

--- robot.py
+++ robot.py
@@ -28,14 +28,14 @@ class ROBOT:
         for jointName in pyrosim.jointNamesToIndices:
             self.motors[jointName] = MOTOR(jointName)
 
-    def Act(self, jointName):
+    def Act(self, t):
         for neuronName in self.nn.Get_Neuron_Names():
             if self.nn.Is_Motor_Neuron(neuronName):
                 jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
                 desiredAngle = self.nn.Get_Value_Of(neuronName)
                 #print(type(self.robotId))
                 #breakpoint()
-                self.motors[jointName].Set_Value(desiredAngle, self.robotId)
+                self.motors[jointName].Set_Value( self.robotId, desiredAngle)
                 print(neuronName, jointName, desiredAngle)
 
         #for i in self.motors:
--- simulation.py
+++ simulation.py
@@ -22,7 +22,7 @@ class SIMULATION:
             self.robot.Sense(t)
             self.robot.Think()
             self.robot.Act(t)
-            time.sleep(1 / 60)
+            time.sleep(1 / 600)
 
     def __del__(self):
         p.disconnect()
